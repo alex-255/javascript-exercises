73ce63b5d0aeee05e0ecc1f8c6a982972a8a9809
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-01-27 22:26:12 +0100

    03_reverseString task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  const arr = str.split("");
+  arr.reverse();
+  const newString = arr.join("");
+  return newString;
 };
 
 // Do not edit below this line
