b13b9efe84e7d53c257f5f115472d11dd95c3b78
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-01-27 22:54:12 +0100

    04_removeFromArray task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toBeRemoved) {
+  for (const num of toBeRemoved) {
+    let index = arr.indexOf(num);
+    while (index > -1) {
+      arr.splice(index, 1);
+      index = arr.indexOf(num);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
