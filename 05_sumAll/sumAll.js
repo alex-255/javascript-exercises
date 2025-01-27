4013b8b311b7ba758e5dfec3eb68d8895649d6e7
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-01-27 23:11:07 +0100

    05_sumAll task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  if (startNum > endNum) {
+    let temp = startNum;
+    startNum = endNum;
+    endNum = temp;
+  }
 
+  if (
+    startNum < 0 ||
+    endNum < 0 ||
+    typeof startNum != "number" ||
+    typeof endNum != "number" ||
+    startNum % 1 != 0 ||
+    endNum % 1 != 0
+  ) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  for (; startNum <= endNum; startNum++) {
+    sum += startNum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
