dfd3f8f02ddd3ad72a37fed940df812ac13f17f1
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-01-28 04:45:29 +0100

    07 termConversation task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheitTemp) {
+  const celsTemp = ((farenheitTemp - 32) * 5) / 9;
+  return Math.round(celsTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsTemp) {
+  const farenheitTemp = (celsTemp * 9) / 5 + 32;
+  return Math.round(farenheitTemp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
