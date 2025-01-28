d58857394f208dd09804445b52712bdb5de758ef
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-01-28 04:48:32 +0100

    07 comments added

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 const convertToCelsius = function (farenheitTemp) {
   const celsTemp = ((farenheitTemp - 32) * 5) / 9;
+  // rounds to .1 decimal point (trick)
   return Math.round(celsTemp * 10) / 10;
 };
 
 const convertToFahrenheit = function (celsTemp) {
   const farenheitTemp = (celsTemp * 9) / 5 + 32;
+  // rounds to .1 decimal point (trick)
   return Math.round(farenheitTemp * 10) / 10;
 };
 
