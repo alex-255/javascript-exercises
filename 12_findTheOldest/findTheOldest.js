ee8f11758ef5d1a992cdc99ae3c8fa2c66c2d17e
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-01-29 23:15:39 +0100

    12 findTheOLDEst task completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const arrNameAndAge = arr.map((person) => {
+    if (person.yearOfDeath === undefined) {
+      return {
+        name: person.name,
+        age: new Date().getFullYear() - person.yearOfBirth,
+      };
+    }
 
+    return {
+      name: person.name,
+      age: person.yearOfDeath - person.yearOfBirth,
+    };
+  });
+
+  const arrAge = arrNameAndAge.map((person) => {
+    return person.age;
+  });
+
+  const max = Math.max(...arrAge);
+
+  let resultIndex = arrAge.indexOf(max);
+
+  return arrNameAndAge[resultIndex];
 };
 
 // Do not edit below this line
