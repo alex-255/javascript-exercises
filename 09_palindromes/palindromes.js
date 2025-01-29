05de349b7af431213e1f2ca8ee9023485d356eaf
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-01-29 20:15:37 +0100

    09_palindromes task completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+  phrase = phrase.toLowerCase();
+  phrase = phrase.replace(/[^a-z0-9]/g, "");
+  const phraseLength = phrase.length;
 
+  if (phraseLength % 2 === 0) {
+    let firstPartStr = phrase.substring(0, phraseLength / 2);
+
+    let secondPartStr = phrase
+      .substring(phraseLength / 2)
+      .split("")
+      .reverse()
+      .join("");
+
+    return firstPartStr === secondPartStr;
+  } else {
+    let firstPartStr = phrase.substring(0, Math.floor(phraseLength / 2));
+
+    let secondPartStr = phrase
+      .substring(Math.ceil(phraseLength / 2))
+      .split("")
+      .reverse()
+      .join("");
+
+    return firstPartStr === secondPartStr;
+  }
 };
 
 // Do not edit below this line
