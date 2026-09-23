cde67f0e4c43abfdaba2578ae7f6c51e60366cad
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-23 15:51:16 +0200

    Solve 4th exercise in Loops and Arrays

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (firstNum, lastNum) {
+  if (
+    !Number.isInteger(firstNum) ||
+    !Number.isInteger(lastNum) ||
+    firstNum < 0 ||
+    lastNum < 0
+  ) {
+    return "ERROR";
+  }
 
+  if (firstNum > lastNum) {
+    const temp = lastNum;
+    lastNum = firstNum;
+    firstNum = temp;
+  }
+
+  let sum = 0;
+  for (let i = firstNum; i <= lastNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
