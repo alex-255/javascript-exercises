7724c3de92e4b7b8da12afde6bc89d87162f9dca
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-23 16:15:34 +0200

    Solve 6th exercise in Loops and Arrays

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+  if (celsius % 1 !== 0) {
+    return parseFloat(celsius.toFixed(1));
+  } else {
+    return celsius;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  if (fahrenheit % 1 !== 0) {
+    return parseFloat(fahrenheit.toFixed(1));
+  } else {
+    return fahrenheit;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
